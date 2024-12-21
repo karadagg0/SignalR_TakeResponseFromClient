12178986f1f77d4a23cfd3c9b4165f68ba1424b9
Ekin Karadağ <ekin.karadag@folyazilim.com>
2024-12-22 01:23:05 +0300

    Proje dosyası ekle.

---
+++ SignalR_Client_/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+
+HubConnection conn = new HubConnectionBuilder()
+                        .WithUrl("https://localhost:7082/myhub")
+                        .WithAutomaticReconnect()
+                        .Build();
+await conn.StartAsync();
+
+Console.WriteLine($"ConnectionId : {conn.ConnectionId}");
+
+conn.On<string, string>("receive", message =>
+{
+    Console.WriteLine($"Message : {message}");
+    return $"Client_OK -  ClientId: {conn.ConnectionId}";
+});
+while (true)
+{
+    if (Console.ReadKey().Key == ConsoleKey.M)
+    {
+        Console.WriteLine("Write ConnId");
+        Console.WriteLine("Connection Id : ");
+        string connId = Console.ReadLine() ?? "";
+        Console.WriteLine("Send Message");
+        Console.WriteLine("Message : ");
+        string message = Console.ReadLine() ?? "";
+        Console.WriteLine();
+        await conn.InvokeAsync("SendMessageAsync", message,connId);
+    }
+}
+
---
+++ SignalR_Take_Response_From_Client/Hubs/MyHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR_Take_Response_From_Client.Hubs
+{
+    public class MyHub:Hub
+    {
+        public async Task SendMessageAsync(string message,string connectionId)
+        {
+            string log =  await Clients.Client(connectionId).InvokeAsync<string>("receive",message,new CancellationToken());
+            await Clients.All.SendAsync("receive",message);
+            //logging, db operations etc
+            Console.WriteLine(log);
+        }
+    }
+}
